bd1f9b1f7b0a27e78261b7bccabf3ee7896a89b9
cmampaey <cedricmampaey@gmail.com>
2023-08-22 08:14:22 +0200

    The demo file for Python for the Coding in the Sun

---
+++ python.py
@@ -0,0 +1,10 @@
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
+print("testing the coding in the sun shtuff")
